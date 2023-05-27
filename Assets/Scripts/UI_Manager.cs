c09039b4d66c55b73591cc493711fe9b768cc826
nnebbe1 <103109022+nnebbe1@users.noreply.github.com>
2023-05-28 00:07:38 +0200

    Ui beginning

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class UI_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Text 
 }
